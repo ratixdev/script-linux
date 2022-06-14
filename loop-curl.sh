32f55007f931fc9d9764de8c612d9696e946a6a8
Ferry Ananda Febian <ratix@ratix>
2022-06-14 00:36:10 -0400

    update script

---
+++ loop-curl.sh
@@ -0,0 +1,3 @@
+while true;
+do curl -I https://www.ratixengineer.com/;
+date ; sleep 0; done
---
+++ manipulasi-count-web.sh
@@ -0,0 +1,3 @@
+while true;
+do wget https://www.ratixengineer.com/;rm -r index.html;curl -I https://www.ratixengineer.com;
+date ; sleep 0; done
